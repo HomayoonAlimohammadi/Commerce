a124a931b2accec52b56716df2fa8545b5792525
Homayoon Alimohammadi <homayoonalimohammadi@gmail.com>
2022-03-23 18:09:07 +0430

    Add Postgresql, Add dump/data.json

--- Commerce/settings.py
+++ Commerce/settings.py
@@ -93,8 +93,12 @@ WSGI_APPLICATION = 'Commerce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'commerce_sql_db',
+        'USER': 'postgres',
+        'PASSWORD': 'pazzo1991', # Change to .env file data
+        'HOST': 'localhost',
+        'PORT': '', # Not needed since the default 
     }
 }
 
